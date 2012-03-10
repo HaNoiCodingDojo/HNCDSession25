964036981d00c519ba386ca9630613b05ebf9d11
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2012-03-10 14:51:46 +0700

    bbayol

--- CountdownKata.py
+++ CountdownKata.py
@@ -5,6 +5,8 @@ import unittest
 def countdownFrom(startPoint):
 	if startPoint == 1:
 		return [1, 0]
+	if startPoint == 2:
+		return [2, 1, 0]
 	else:
 		return [startPoint]
 
@@ -16,6 +18,8 @@ class CountdownKataTest(unittest.TestCase):
 		self.assertEqual([0], countdownFrom(0))
 	def test_1_return_an_array_with_1_and_0(self):
 		self.assertEqual([1, 0], countdownFrom(1))
+	def test_2_return_an_array_with_2_and_1_and_0(self):
+		self.assertEqual([2, 1, 0], countdownFrom(2))
 
 if __name__ == "__main__":
 	unittest.main()
