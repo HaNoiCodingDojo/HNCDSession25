b80dca86b4f60645048125b19c982732565c5f52
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2012-03-10 15:19:13 +0700

    cmpitg

--- CountdownKata.py
+++ CountdownKata.py
@@ -9,9 +9,6 @@ def countdownFrom(startPoint):
 	return result
 
 class CountdownKataTest(unittest.TestCase):
-	def test_method(self):
-		self.assertEqual(True, True)
-
 	def test_0_return_an_array_with_0(self):
 		self.assertEqual([0], countdownFrom(0))
 
