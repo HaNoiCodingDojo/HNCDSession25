1a7f00c7af4c81fd60a6c96dce31287e5664d4aa
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2012-03-10 14:41:41 +0700

    SergeStinckwich

--- CountdownKata.py
+++ CountdownKata.py
@@ -2,10 +2,13 @@
 
 import unittest
 
-class TrueTest(unittest.TestCase):
+def 
+class CountdownKataTest(unittest.TestCase):
 	def test_method(self):
 		self.assertEqual(True, True)
 
+	def test_0_return_an_array_with_0(self):
+		self.assertEqual([0], countdownFrom(0))
 
 if __name__ == "__main__":
 	unittest.main()
